9dee65d2f35891ef19045032277f0d4706f1bf81
gowthami-krishnan <108142949+gowthami-krishnan@users.noreply.github.com>
2022-06-27 19:22:26 +0530

    Add files via upload

---
+++ Vehicle.java
@@ -0,0 +1,27 @@
+package Week3;
+
+public class Vehicle {
+
+	public void applyBreak() {
+		System.out.println("Break appilied");
+	}
+	
+	public void soundHorn() {
+		System.out.println("Sound is visible");
+	}
+	public static void main(String[] args) {
+		
+		BMW obj=new BMW();
+		obj.applyBreak();
+		
+		Bajaj obj1=new Bajaj();
+		obj1.handStart();
+	}
+	
+	
+		
+		
+
+	}
+
+
