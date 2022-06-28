aa55bf41ed76f44a3ca6944e8f2387e3ddc491b1
gowthami-krishnan <108142949+gowthami-krishnan@users.noreply.github.com>
2022-06-28 19:07:14 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,19 @@
+package org.college;
+
+import org.department.Department;
+
+public class College extends Department {
+	
+	public void collegeName() {
+		System.out.println("My College Name is GuruNanak College");
+	}
+	
+	public void collegeCode() {
+		System.out.println("GNIT 1453");
+	}
+	
+	public void collegeRank() {
+		System.out.println("5th Rank");
+	}
+
+}
